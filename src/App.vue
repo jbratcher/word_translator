0af38c577a39dcd52ce8dae7119dc874f02b8553
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-11-13 12:48:11 +0000

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="container">
+    <div class="row mt-5 justify-content-center text-center">
+      <div class="col-md-9">
+        <h1 class="pt-3 text-uppercase">Word Translator</h1>
+        <h5 class="lead pb-5">Powered by Vue.js & Yandex Translate API</h5>
+        <TranslateForm  v-on:formSubmit="translateText"></TranslateForm>
+        <TranslateOutput v-text="translatedText"></TranslateOutput>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import TranslateForm from "./components/TranslateForm"
+import TranslateOutput from "./components/TranslateOutput"
+
+export default {
+  name: "app",
+  components: {
+    TranslateForm, TranslateOutput
+  },
+  data: function() {
+    return {
+      translatedText: ""
+    }
+  },
+  methods: {
+    translateText: function(text, language) {
+    this.$http.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170927T013614Z.0398812bbce63b32.71a2a2d1a3dc29ad889789d7ca354e2439180293&lang="+language+"&text=" + text)
+    .then((response) => {
+      this.translatedText = response.body.text[0]
+    })
+    }
+  }
+}
+
+</script>
+
+<style src="../src/assets/styles.css"  type="text/css">
+  
+</style>
