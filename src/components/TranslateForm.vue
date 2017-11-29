cc2fc7a864bd00667658d2f832a3e99ec0052e99
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-11-29 12:24:16 +0000

    initial commit

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,37 @@
+<template>
+        <form id="transForm" v-on:submit="formSubmit">
+            <input class="form-control form-control-lg pl-3" type="text" v-model="textToTranslate" placeholder="Enter word to translate">
+            <select class="form-control form-control-lg pl-3" v-model="language">
+                <option value="es">Spanish</option>
+                <option value="fr">French</option>
+                <option value="zh">Chinese</option>
+                <option value="ga">Irish</option>
+                <option value="ru">Russian</option>
+            </select>
+            <input class="btn btn-primary btn-large d-block w-100" type="submit" value="Translate">
+        </form>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data() {
+      return {
+          textToTranslate:""
+      }
+  },
+  created() {
+      this.language = "es"
+  },
+  methods: {
+      formSubmit(e) {
+          this.$emit("formSubmit", this.textToTranslate, this.language)
+          e.preventDefault();
+      }
+  }
+}
+
+</script>
+
+<style src="../assets/styles.css"  type="text/css"></style>
---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2 class="display-1 mt-1" id="TranslateOutput" >{{translatedText}}</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'translateOutput',
+  props: ['translatedText']
+}
+
+</script>
+
+<style src="../assets/styles.css"  type="text/css"></style>
