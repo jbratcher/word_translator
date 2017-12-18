629ce3500428cdb4227aeb54d20421df987df50f
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-12-18 00:54:50 +0000

    theming

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 8080, //c9.io port
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
